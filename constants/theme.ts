0e8fc2382e1a1bde2895ade2eba8a6798d22dc4d
Guilherme Cunha <guicunhasou@gmail.com>
2026-06-08 11:49:21 -0300

    feat: update theme colors

--- constants/theme.ts
+++ constants/theme.ts
@@ -5,24 +5,24 @@
 
 import { Platform } from 'react-native';
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#7B61FF';
+const tintColorDark = '#B9A7FF';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#17142A',
+    background: '#F8F6FF',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#5F6078',
+    tabIconDefault: '#5F6078',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#F7F4FF',
+    background: '#0B1026',
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: '#B8BDE0',
+    tabIconDefault: '#B8BDE0',
     tabIconSelected: tintColorDark,
   },
 };
